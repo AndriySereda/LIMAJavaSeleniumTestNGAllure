dec5e5412ba6d5620eb494a42999983a15dfa288
AndreySereda <andreysereda77@gmail.com>
2024-06-03 20:07:16 +0200

    Add first test

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,12 @@
+ import org.testng.annotations.Test;
+
+/**
+ * Created by User: Andrey Sereda
+ */
+public class TestTest {
+    // запускается пустой тест для проверки того, что все библиотеки были подгружены
+    @Test
+    public void test(){
+
+    }
+}
